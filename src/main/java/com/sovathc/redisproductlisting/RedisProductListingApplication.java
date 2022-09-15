d6621fdf6d826f89b67b7652b316dbfdcce044fe
SovathChean <sovathchean09@gmail.com>
2022-09-15 10:24:31 +0700

    init

---
+++ src/main/java/com/sovathc/redisproductlisting/RedisProductListingApplication.java
@@ -0,0 +1,13 @@
+package com.sovathc.redisproductlisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisProductListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisProductListingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sovathc/redisproductlisting/RedisProductListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sovathc.redisproductlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisProductListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
